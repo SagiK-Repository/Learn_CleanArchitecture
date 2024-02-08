515a5aac2d7249da0d741710c7ea66c1a22d0db3
박주형 <juhyung.park@mirero.co.kr>
2024-02-08 11:14:13 +0900

    feat : record 기반 valueobject 추가

---
+++ DDD_Test_Project/DDD_Test_Project/ValueObject/ValueObject_Record.cs
@@ -0,0 +1,15 @@
+﻿namespace DDD_Test_Project.ValueObject_Record;
+
+public record FullName(string FirstName, string LastName);
+
+public record Name
+{
+    public string Value { get; init; }
+
+    public Name(string value)
+    {
+        if (value.Length > 10)
+            throw new ArgumentException("Name cannot be longer than 10 characters.");
+        Value = value;
+    }
+}
