8f4fa74ef5f1609ccc51420d3f48c30cd0a99cb6
박주형 <juhyung.park@mirero.co.kr>
2024-02-08 10:34:35 +0900

    feat : ValueObject 클래스 기반 ValueObject 생성

---
+++ DDD_Test_Project/DDD_Test_Project/ValueObject/ValueObject_ValueObject.cs
@@ -0,0 +1,38 @@
+﻿using DDD_Test_Project.Common;
+
+namespace DDD_Test_Project.ValueObject_ValueObject;
+
+public class FullName : ValueObject
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+
+    public FullName(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return FirstName;
+        yield return LastName;
+    }
+}
+
+public class Name : ValueObject
+{
+    public Name(string value)
+    {
+        if (value.Length > 10)
+            throw new ArgumentException("Name length must be 10 characters or less.");
+
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
