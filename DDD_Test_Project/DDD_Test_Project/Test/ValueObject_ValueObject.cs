a67feaf89c3d5dd3a58338b6b0a375fe5d57e6f9
박주형 <juhyung.park@mirero.co.kr>
2024-02-08 11:13:52 +0900

    test : VolueObject 클래스 추가에 따른 test 추가

--- DDD_Test_Project/DDD_Test_Project/Test/ValueObject_ValueObject.cs
+++ DDD_Test_Project/DDD_Test_Project/Test/ValueObject_ValueObject.cs
@@ -39,4 +39,40 @@ public class ValueObject_ValueObject
         result.Should().Be(expected);
         resultEqual.Should().Be(expected);
     }
+
+    [Theory(DisplayName = "FullName Equality Tests 2")]
+    [InlineData("John", "Doe", "John", "Doe", true)]
+    [InlineData("John", "Doe", "Jane", "Doe", false)]
+    public void FullNameEqualityTests_2(string firstName1, string lastName1, string firstName2, string lastName2, bool expected)
+    {
+        // Arrange
+        var fullName1 = new FullName_2(firstName1, lastName1);
+        var fullName2 = new FullName_2(firstName2, lastName2);
+
+        // Act
+        bool result = fullName1.Equals(fullName2);
+        bool resultEqual = fullName1 == fullName2;
+
+        // Assert
+        result.Should().Be(expected);
+        resultEqual.Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Name Equality Tests 2")]
+    [InlineData("John", "John", true)]
+    [InlineData("John", "Jane", false)]
+    public void NameEqualityTests_2(string value1, string value2, bool expected)
+    {
+        // Arrange
+        var name1 = new Name_2(value1);
+        var name2 = new Name_2(value2);
+
+        // Act
+        bool result = name1.Equals(name2);
+        bool resultEqual = name1 == name2;
+
+        // Assert
+        result.Should().Be(expected);
+        resultEqual.Should().Be(expected);
+    }
 }
