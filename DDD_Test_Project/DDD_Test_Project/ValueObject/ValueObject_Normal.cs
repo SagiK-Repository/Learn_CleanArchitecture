0a0084bf52d3696736e4834acd6f36e576a80897
박주형 <juhyung.park@mirero.co.kr>
2024-02-08 09:50:31 +0900

    feat : Name 값 객체 추가

--- DDD_Test_Project/DDD_Test_Project/ValueObject/ValueObject_Normal.cs
+++ DDD_Test_Project/DDD_Test_Project/ValueObject/ValueObject_Normal.cs
@@ -71,3 +71,57 @@ public class FullName_IEquatable : IEquatable<FullName_IEquatable>
         }
     }
 }
+public class Name : IEquatable<Name>
+{
+    public Name(string value)
+    {
+        if (value.Length > 10)
+        {
+            throw new ArgumentException("Name length must be 10 characters or less.");
+        }
+
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    public bool Equals(Name other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return string.Equals(Value, other.Value);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Name)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return (Value != null ? Value.GetHashCode() : 0);
+    }
+
+    public static bool operator ==(Name name1, Name name2)
+    {
+        if (ReferenceEquals(name1, name2))
+        {
+            return true;
+        }
+
+        if (ReferenceEquals(null, name1) || ReferenceEquals(null, name2))
+        {
+            return false;
+        }
+
+        return name1.Equals(name2);
+    }
+
+    public static bool operator !=(Name name1, Name name2)
+    {
+        return !(name1 == name2);
+    }
+}
