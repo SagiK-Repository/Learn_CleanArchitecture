faa388f5f61344af086cba2b4307cc57872c522e
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-02-12 11:12:17 +0900

    style : object 분리 및 코드 정렬

---
+++ DDD_Test_Project/DDD_Test_Project/Common/Object.cs
@@ -0,0 +1,47 @@
+﻿namespace DDD_Test_Project.Common;
+public abstract class Object
+{
+    protected static bool EqualOperator(Object left, Object right)
+    {
+        if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+        {
+            return false;
+        }
+        return ReferenceEquals(left, right) || left.Equals(right);
+    }
+
+    protected static bool NotEqualOperator(Object left, Object right)
+    {
+        return !(EqualOperator(left, right));
+    }
+
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (Object)obj;
+
+        return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+    public static bool operator ==(Object one, Object two)
+    {
+        return EqualOperator(one, two);
+    }
+
+    public static bool operator !=(Object one, Object two)
+    {
+        return NotEqualOperator(one, two);
+    }
+}
--- DDD_Test_Project/DDD_Test_Project/Test/Object/Object.cs
+++ DDD_Test_Project/DDD_Test_Project/Test/Object/Object.cs
@@ -1,87 +1,86 @@
 ﻿using DDD_Test_Project.Object;
 
-namespace DDD_Test_Project.Test.Object
+namespace DDD_Test_Project.Test.Object;
+
+public class Object
 {
-    public class Object
+    [InlineData(1, 1.0)] // 일반 부여
+    [InlineData(4000, 2000.0)] // 최대 초과
+    [InlineData(-2000, 0.0)] // 최소 초과
+    [Theory(DisplayName = "DDD Value : Artist.Width<int> Value Test")]
+    public void DDDTest_Width_int_Test(object value, double outValue)
     {
-        [InlineData(1, 1.0)] // 일반 부여
-        [InlineData(4000, 2000.0)] // 최대 초과
-        [InlineData(-2000, 0.0)] // 최소 초과
-        [Theory(DisplayName = "DDD Value : Artist.Width<int> Value Test")]
-        public void DDDTest_Width_int_Test(object value, double outValue)
+        // Arange
+
+        // Act
+        var width = new Width<int>(Convert.ToInt32(value));
+
+        // Assert
+        width.GetValue().Should().Be(outValue);
+    }
+
+    [InlineData(1.0, 1.0)] // 일반 부여
+    [InlineData(4000.0, 2000.0)] // 최대 초과
+    [InlineData(-2000.0, 0.0)] // 최소 초과
+    [Theory(DisplayName = "DDD Value : Artist.Width<double> Value Test")]
+    public void DDDTest_Width_double_Test(double value, double outValue)
+    {
+        // Arange
+
+        // Act
+        var width = new Width<double>(value);
+
+        // Assert
+        width.GetValue().Should().Be(outValue);
+    }
+
+    [InlineData(1.0, 0.0, 2.0, 1.0)] // 일반 부여
+    [InlineData(1.0, 100.0, 200.0, 100.0)] // 최소 초과
+    [InlineData(1.0, -100.0, -10.0, -10.0)] // 최대 초과
+    [Theory(DisplayName = "DDD Value : Artist.Width<double> MinMax Value Test")]
+    public void DDDTest_Width_double_MinMax_Test(double value, double minValue, double maxValue, double outValue)
+    {
+        // Arange
+
+        // Act
+        var width = new Width<double>(value, minValue, maxValue);
+
+        // Assert
+        width.GetValue().Should().Be(outValue);
+    }
+
+    [InlineData(1.0, 4000.0, 2.0, 1.0, "입력 값이 2000보다 큽니다.")] // 최소 예외
+    [InlineData(1.0, 0.0, -100.0, 1.0, "입력 값이 0보다 작습니다.")] // 최대 예외
+    [Theory(DisplayName = "DDD Value : Artist.Width<double> Exception Test")]
+    public void DDDTest_Width_double_Exception_Test(double value, double minValue, double maxValue, double outValue, string exceptionMessage)
+    {
+        // Arange
+        string exception = string.Empty;
+        var width = new Width<double>(value);
+
+        // Act
+        try
         {
-            // Arange
-
-            // Act
-            var width = new Width<int>(Convert.ToInt32(value));
-
-            // Assert
-            width.GetValue().Should().Be(outValue);
+            width.SetMinMaxValue(minValue, maxValue);
+        }
+        catch (Exception ex)
+        {
+            exception = ex.Message;
         }
 
-        [InlineData(1.0, 1.0)] // 일반 부여
-        [InlineData(4000.0, 2000.0)] // 최대 초과
-        [InlineData(-2000.0, 0.0)] // 최소 초과
-        [Theory(DisplayName = "DDD Value : Artist.Width<double> Value Test")]
-        public void DDDTest_Width_double_Test(double value, double outValue)
-        {
-            // Arange
+        // Assert
+        width.GetValue().Should().Be(outValue);
+        exception.Should().Be(exceptionMessage);
+    }
 
-            // Act
-            var width = new Width<double>(value);
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(2, 1)]
+    public void Width_Constructor_ShouldThrowException(double minValue, double maxValue)
+    {
+        // Arrange
 
-            // Assert
-            width.GetValue().Should().Be(outValue);
-        }
-
-        [InlineData(1.0, 0.0, 2.0, 1.0)] // 일반 부여
-        [InlineData(1.0, 100.0, 200.0, 100.0)] // 최소 초과
-        [InlineData(1.0, -100.0, -10.0, -10.0)] // 최대 초과
-        [Theory(DisplayName = "DDD Value : Artist.Width<double> MinMax Value Test")]
-        public void DDDTest_Width_double_MinMax_Test(double value, double minValue, double maxValue, double outValue)
-        {
-            // Arange
-
-            // Act
-            var width = new Width<double>(value, minValue, maxValue);
-
-            // Assert
-            width.GetValue().Should().Be(outValue);
-        }
-
-        [InlineData(1.0, 4000.0, 2.0, 1.0, "입력 값이 2000보다 큽니다.")] // 최소 예외
-        [InlineData(1.0, 0.0, -100.0, 1.0, "입력 값이 0보다 작습니다.")] // 최대 예외
-        [Theory(DisplayName = "DDD Value : Artist.Width<double> Exception Test")]
-        public void DDDTest_Width_double_Exception_Test(double value, double minValue, double maxValue, double outValue, string exceptionMessage)
-        {
-            // Arange
-            string exception = string.Empty;
-            var width = new Width<double>(value);
-
-            // Act
-            try
-            {
-                width.SetMinMaxValue(minValue, maxValue);
-            }
-            catch (Exception ex)
-            {
-                exception = ex.Message;
-            }
-
-            // Assert
-            width.GetValue().Should().Be(outValue);
-            exception.Should().Be(exceptionMessage);
-        }
-
-        [Theory]
-        [InlineData(1, 0)]
-        [InlineData(2, 1)]
-        public void Width_Constructor_ShouldThrowException(double minValue, double maxValue)
-        {
-            // Arrange
-
-            // Act & Assert
-            Assert.Throws<ArgumentException>(() => new Width<double>(0.0f, minValue, maxValue));
-        }
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new Width<double>(0.0f, minValue, maxValue));
     }
 }
