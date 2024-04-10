a4f2c675c732a9ddb3a88a3b36c23e20f7c83ba9
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-11 07:41:33 +0900

    feat : add movie repository

--- Clean Architecture/CleanMovie.Domain/Movie.cs
+++ Clean Architecture/CleanMovie.Domain/Movie.cs
@@ -10,6 +10,6 @@ namespace CleanMovie.Domain
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
     }
 }
--- Clean Architecture/CleanMovie.Infrastructure/MovieRepository.cs
+++ Clean Architecture/CleanMovie.Infrastructure/MovieRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace CleanMovie.Infrastructure
 {
-    public class MovieRepository
+    public class MovieRepository : IMovieRepository
     {
+        public static List<Movie> movies = new List<Movie>()
+        {
+            new Movie { Id = 1, Name = "Passion of Christ", Cost = 2 },
+            new Movie { Id = 2, Name = "Home Alone 4", Cost = 3 }
+        };
+
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
     }
 }
