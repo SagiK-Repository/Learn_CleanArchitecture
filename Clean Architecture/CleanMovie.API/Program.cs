d47ed6429567545f480e434ee05f11f827f5c547
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-11 19:34:03 +0900

    feat : movie DB 설정 구성

--- Clean Architecture/CleanMovie.API/Program.cs
+++ Clean Architecture/CleanMovie.API/Program.cs
@@ -1,7 +1,12 @@
 using CleanMovie.Application;
 using CleanMovie.Infrastructure;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Register Configuration
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -9,6 +14,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Database Service
+builder.Services.AddDbContext<MovieDBContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+    b => b.MigrationsAssembly("CleanMovie.API")));
 builder.Services.AddScoped<IMovieService, MovieService>();
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 
