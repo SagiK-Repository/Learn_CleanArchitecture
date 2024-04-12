9256844f3f929c402538afebe0762117dabcde3f
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-12 12:46:01 +0900

    feat : add CleanArchitecture Infrastructure DI

---
+++ CleanArchitecture.DI/CleanArchitecture.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection ImplementPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MovieRentalDBContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), 
+                b => b.MigrationsAssembly(typeof(MovieRentalDBContext).Assembly.FullName)), ServiceLifetime.Transient);
+
+            services.AddScoped<IMovieRentalDBContext>(provider => provider.GetService<MovieRentalDBContext>());
+
+            return services;
+        }
+    }
+}
+    
---
+++ CleanArchitecture.DI/CleanArchitecture.Infrastructure/MovieRentalDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public class MovieRentalDBContext : DbContext, IMovieRentalDBContext
+    {
+        public MovieRentalDBContext(DbContextOptions<MovieRentalDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
