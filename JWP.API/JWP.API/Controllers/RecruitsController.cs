0e9221196b83c8747bb6834dbb73229d87037ddc
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-13 08:31:29 +0900

    feat : 승인 부여

--- JWP.API/JWP.API/Controllers/RecruitsController.cs
+++ JWP.API/JWP.API/Controllers/RecruitsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JWP.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class RecruitsController : ControllerBase
