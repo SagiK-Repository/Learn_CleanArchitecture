95fb718cfd4c6029904278e6b689b23c45efbaaf
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-13 14:57:17 +0900

    feat : add mediatR

---
+++ MovieManagement.API/MovieManagement.API/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Library.Models;
+using MovieManagement.Library.Queries;
+
+namespace MovieManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MoviesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // Get: api/<MoviesController>
+        [HttpGet]
+        public async Task<List<MovieModel>> Get()
+            => await _mediator.Send(new GetMovieListQuery());
+    }
+}
--- MovieManagement.API/MovieManagement.API/Program.cs
+++ MovieManagement.API/MovieManagement.API/Program.cs
@@ -1,3 +1,7 @@
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using MovieManagement.Library.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDataRepository, DataRepository>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(DataRepository).Assembly));
 
 var app = builder.Build();
 
