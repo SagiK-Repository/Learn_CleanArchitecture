f7fbc08808dfcae8563c78b43d7354f988952692
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-20 19:31:24 +0900

    feat : add datacontext

---
+++ Entity_Framework_Core_for_Beginners/Entity_Framework_Core_for_Beginners/Data/ContosoPizzaContext.cs
@@ -0,0 +1,15 @@
+﻿using Entity_Framework_Core_for_Beginners.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity_Framework_Core_for_Beginners.Data;
+
+public class Entity_Framework_Core_for_BeginnersContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; } = null!;
+    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(@"Data Source =(localdb)\ProjectModels;Initial Catalog=ContosoPizza-Part1;Integrated Security=True;");
+}
--- Entity_Framework_Core_for_Beginners/Entity_Framework_Core_for_Beginners/Program.cs
+++ Entity_Framework_Core_for_Beginners/Entity_Framework_Core_for_Beginners/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Entity_Framework_Core_for_Beginners.Data;
+using Entity_Framework_Core_for_Beginners.Models;
+using Entity_Framework_Core_for_BeginnersContext context = new();
+
+Product veggieSpecial = new Product()
+{
+    Name = "Veggie Special Pizza",
+    Price = 9.99M
+};
+context.Products.Add(veggieSpecial);
+
+Product deluxeMeat = new Product()
+{
+    Name = "Deluxe Meat Pizza",
+    Price = 12.99M
+};
+context.Products.Add(deluxeMeat);
+
+context.SaveChanges();
