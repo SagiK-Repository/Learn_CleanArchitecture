73da0c6dd94c13466e196201a6faef7aa615c856
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-20 22:55:31 +0900

    chore : copy ExisitingDB3API to ExisitingDB5API

---
+++ Entity_Framework_Core_for_Beginners/ExisitingDB5API/Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ExisitingDB3API.Data;
+using ExisitingDB3API.Models;
+
+namespace ExisitingDB3API.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ExisitingDB3API.Data.ContosoPizzaPart1Context _context;
+
+        public DeleteModel(ExisitingDB3API.Data.ContosoPizzaPart1Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Product = product;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                Product = product;
+                _context.Products.Remove(Product);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Entity_Framework_Core_for_Beginners/ExisitingDB5API/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ExisitingDB3API.Data;
+using ExisitingDB3API.Models;
+
+namespace ExisitingDB3API.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ExisitingDB3API.Data.ContosoPizzaPart1Context _context;
+
+        public DetailsModel(ExisitingDB3API.Data.ContosoPizzaPart1Context context)
+        {
+            _context = context;
+        }
+
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
