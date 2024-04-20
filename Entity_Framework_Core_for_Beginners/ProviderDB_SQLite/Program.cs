99e814f6a537922bc2d67de992f769523622eb5b
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-20 22:30:29 +0900

    feat : add 기본 project

---
+++ Entity_Framework_Core_for_Beginners/ProviderDB_SQLite/Program.cs
@@ -0,0 +1,20 @@
+﻿using ProviderDB_SQLite.Data;
+using ProviderDB_SQLite.Models;
+
+using ContosoPizzaPart1Context context = new();
+
+Product veggieSpecial = new Product()
+{
+    Name = "Veggie Special Pizza",
+    Price = 9.99M
+};
+context.Products.Add(veggieSpecial);
+
+Product deluxeMeat = new Product()
+{
+    Name = "Deluxe Meat Pizza",
+    Price = 12.99M
+};
+context.Products.Add(deluxeMeat);
+
+context.SaveChanges();
