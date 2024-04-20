8dacad16760260de7b69a151502781f5fcc6cc6c
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-20 23:01:26 +0900

    feat : add customer scoffold item

---
+++ Entity_Framework_Core_for_Beginners/ExisitingDB5API/Pages/Customers/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ExisitingDB3API.Data;
+using ExisitingDB3API.Models;
+
+namespace ExisitingDB5API.Pages.Customers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ExisitingDB3API.Data.ContosoPizzaPart1Context _context;
+
+        public DeleteModel(ExisitingDB3API.Data.ContosoPizzaPart1Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Customer = customer;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                Customer = customer;
+                _context.Customers.Remove(Customer);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Entity_Framework_Core_for_Beginners/ExisitingDB5API/Pages/Customers/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ExisitingDB3API.Data;
+using ExisitingDB3API.Models;
+
+namespace ExisitingDB5API.Pages.Customers
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ExisitingDB3API.Data.ContosoPizzaPart1Context _context;
+
+        public DetailsModel(ExisitingDB3API.Data.ContosoPizzaPart1Context context)
+        {
+            _context = context;
+        }
+
+        public Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Customer = customer;
+            }
+            return Page();
+        }
+    }
+}
