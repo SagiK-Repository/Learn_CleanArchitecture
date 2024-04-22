190ed8fc5953f9b00f30f68cec8510f94c2edfc2
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-22 11:57:51 +0900

    feat : update client

--- GrpcDemo/GrpcClient/Program.cs
+++ GrpcDemo/GrpcClient/Program.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var input = new HelloRequest { Name = "Tim" };
+
+            var chanel = GrpcChannel.ForAddress("https://localhost:7193");
+            var client = new Greeter.GreeterClient(chanel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
             Console.ReadLine();
         }
     }
