35b68afb9348b1b70aa8caa84194b6164e2b8822
박주형 <juhyung.park@mirero.co.kr>
2024-10-10 19:55:25 +0900

    feat : add docker grpc server

---
+++ ImageHandlerService/기초 구현/DockerGrpcServer/Services/HelloService.cs
@@ -0,0 +1,15 @@
+﻿using DockerGrpcServer.Protos;
+using Grpc.Core;
+
+namespace DockerGrpcServer.Services;
+
+public class HelloService : Hello.HelloBase
+{
+    public HelloService() { }
+
+    public override async Task<HelloReply> Hello(HelloRequest request, ServerCallContext context)
+    {
+        await Task.CompletedTask;
+        return new HelloReply() { Reply = "Hello World!" };
+    }
+}
