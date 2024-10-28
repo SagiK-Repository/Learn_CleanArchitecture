73aaf04b5c0eb64c2ceb5c2b35853fdf0fbcd064
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-10-28 22:55:04 +0900

    chore : server 설정 개선

--- ImageHandlerService/기초 구현/DockerGrpcServer2_1/Program.cs
+++ ImageHandlerService/기초 구현/DockerGrpcServer2_1/Program.cs
@@ -30,7 +30,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<Hello1Service>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-app.Run();
+
+var serverTask = Task.Run(() => app.Run());
 
 var printService = app.Services.GetRequiredService<IPrint>();
 await printService.Print("Hello World!");
+
+await serverTask;
--- ImageHandlerService/기초 구현/DockerGrpcServer2_2/Program.cs
+++ ImageHandlerService/기초 구현/DockerGrpcServer2_2/Program.cs
@@ -31,7 +31,9 @@ var app = builder.Build();
 app.MapGrpcService<Hello2Service>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-app.Run();
+var serverTask = Task.Run(() => app.Run());
 
 var printService = app.Services.GetRequiredService<IPrint>();
 await printService.Print("Hello World!");
+
+await serverTask;
