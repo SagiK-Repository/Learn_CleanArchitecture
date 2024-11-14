44b3a110f5f4e8347b23b9c6e1480ad42ab3d9d2
박주형 <juhyung.park@mirero.co.kr>
2024-11-14 16:59:28 +0900

    feat : add postgre 설정

---
+++ ImageHandlerService/Docker DB/DockerDBServer/DBContext/SampleContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DockerDBServer.DBContext;
+
+public class SampleContext : DbContext
+{
+    public SampleContext() { }
+
+    public SampleContext(DbContextOptions<SampleContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("User ID=postgres;Password=P@ss0rd;Host=localhost;Port=5432;Database=ImageHandlerServiceDB;");
+}
