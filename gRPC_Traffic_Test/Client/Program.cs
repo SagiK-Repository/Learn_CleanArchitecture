ece94a5174bf07607823ef19520336e38bf76410
SAgiKPJH1021 <juhyung1021@gmail.com>
2025-02-25 16:33:33 +0900

    feat : add grpc-test to async

--- gRPC_Traffic_Test/Client/Program.cs
+++ gRPC_Traffic_Test/Client/Program.cs
@@ -5,32 +5,43 @@ using Server;
 var data10MBText = File.ReadAllLines("./Files/5MB.txt");
 string data = string.Join("", data10MBText);
 
+async Task SendDataAsync(int index)
+{
+    for (int ind = 0; ind < 10; ind++)
+    {
+        // SSL 인증서 검증을 비활성화
+        var httpClientHandler = new HttpClientHandler();
+        httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+
+        using var channel = GrpcChannel.ForAddress("http://server:8080", new GrpcChannelOptions { HttpClient = new HttpClient(httpClientHandler), MaxReceiveMessageSize = 50 * 1024 * 1024 /*50MB*/ });
+        var client = new StreamService.StreamServiceClient(channel);
+        using var call = client.StreamData(new StreamingRequest { Message = data });
+
+        await Task.Delay(1500);
+
+        try
+        {
+            while (await call.ResponseStream.MoveNext())
+            {
+                Console.WriteLine($"Receive from request {index} {ind}");
+            }
+            Console.WriteLine($"Receive Complete for request {index} {ind}");
+        }
+        catch (RpcException ex)
+        {
+            Console.WriteLine($"Error in request {index} {ind}: {ex.Status}");
+        }
+    }
+}
+
+
+var tasks = new List<Task>();
+
 for (int index = 0; index < 10; index++)
 {
-    // SSL 인증서 검증을 비활성화
-    var httpClientHandler = new HttpClientHandler();
-    httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
-
-    using var channel = GrpcChannel.ForAddress("http://server:8080", new GrpcChannelOptions { HttpClient = new HttpClient(httpClientHandler), MaxReceiveMessageSize = 50 * 1024 * 1024 /*50MB*/ });
-    var client = new StreamService.StreamServiceClient(channel);
-    using var call = client.StreamData(new StreamingRequest { Message = data });
-
-    await Task.Delay(1500);
-
-    try
-    {
-        while (await call.ResponseStream.MoveNext())
-        {
-            //Console.WriteLine(call.ResponseStream.Current.Response);
-            Console.WriteLine("Receive");
-        }
-        Console.WriteLine("Receive Complete");
-    }
-    catch (RpcException ex)
-    {
-        Console.WriteLine($"Error: {ex.Status}");
-    }
-
+    tasks.Add(SendDataAsync(index));
 }
+
+await Task.WhenAll(tasks);
 
 Console.ReadLine();
